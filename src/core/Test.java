be7ae2d4ce63166aaaea14de780b9dcc842eeb42
Avinash Kamlakar Chauhan <avinashkc@maveric-systems.com>
2024-02-19 10:57:34 +0530

    core java

---
+++ src/core/Test.java
@@ -0,0 +1,13 @@
+package core;
+
+public class Test {
+
+	
+	
+	public static void main(String args[])
+	{
+		TestMethod tm = new TestMethod();
+		tm.testPublic();
+		tm.testProtected();
+	}
+}
---
+++ src/core/TestMethod.java
@@ -0,0 +1,28 @@
+package core;
+
+public class TestMethod {
+
+	public void testPublic()
+	{
+		System.out.println("This is public method");
+		
+	}
+		
+	private void testPrivate()
+	{
+		System.out.println("This is private method");
+	}
+	
+	protected void testProtected()
+	{
+		System.out.println("This is protected method");
+	}
+	
+	
+	public static void main(String args[])
+	{
+		
+		TestMethod tm = new TestMethod();
+		tm.testPrivate();
+	}
+}
